7b5a8f4f8431ea57fdbb7e34cd4a95c9c3a6b5d7
Paras joshi <parasjoshi71996@gmail.com>
2020-11-11 19:03:08 +0530

    Revert "js update"
    This reverts commit b386a6955156ee89d8278ba6f2754f9da5392d73.

--- script.js
+++ script.js
@@ -10,17 +10,17 @@ var sceneEl = document.querySelector('a-scene');
        height:{type : 'number' , default : 5}, 
        depth:{type : 'number' , default : 1},
       color:{type:'color',default:'blue'},
-      mydata : {type:'array'}
+      mydata : {type:'number'}
     },
     init: function () {
       // This will be called after the entity has properly attached and loaded.
       var data = this.data;
-      var pos = this.data.mydata[1];
+     
       //data.width = 10;
       this.geometry = new THREE.BoxBufferGeometry(data.width, data.height, data.depth);
     this.material = new THREE.MeshStandardMaterial({color: data.color});
     this.mesh = new THREE.Mesh(this.geometry, this.material);
-    curr.object3D.position.set(-5*pos, 0, -35*(this.data.mydata[0]));
+    curr.object3D.position.set(-5, 0, -35*(this.data.mydata));
     //entityEl.object3D.position.z += (3*-35);
     curr.object3D.rotation.y = THREE.Math.degToRad(90);
     curr.setObject3D('mesh', this.mesh);
@@ -29,45 +29,16 @@ var sceneEl = document.querySelector('a-scene');
       console.log(this.data.mydata);
     }
   });
-  AFRAME.registerComponent('do-something-once', {
-    schema: {
-      message: {type: 'string', default: 'Hello, World!'},
-      width:{type : 'number' , default : 20},
-       height:{type : 'number' , default : 5}, 
-       depth:{type : 'number' , default : 1},
-      color:{type:'color',default:'blue'},
-      mydata : {type:'array'}
-    },
-    init: function () {
-      // This will be called after the entity has properly attached and loaded.
-      var data = this.data;
-      var pos = this.data.mydata[1];
-      //data.width = 10;
-      this.geometry = new THREE.BoxBufferGeometry(data.width, data.height, data.depth);
-    this.material = new THREE.MeshStandardMaterial({color: data.color});
-    this.mesh = new THREE.Mesh(this.geometry, this.material);
-    lft.object3D.position.set(-5*pos, 0, -35*(this.data.mydata[0]));
-    //entityEl.object3D.position.z += (3*-35);
-    lft.object3D.rotation.y = THREE.Math.degToRad(90);
-    lft.setObject3D('mesh', this.mesh);
-    
-      console.log('I am ready!');
-      console.log(this.data.mydata);
-    }
-  });
- // for(var i=1; i<=3; ++i){
+  for(var i=1; i<=3; ++i){
 
     //console.log(i);
+  (function(i){
     //console.log(i);
-    
-  var curr = document.createElement('a-box');
-  curr.setAttribute('do-something-once-loaded',{mydata:[1,1]});
+    var curr = document.createElement('a-box');
+  curr.setAttribute('do-something-once-loaded',{mydata:i});
   sceneEl.appendChild(curr);
-  var lft = document.createElement('a-box');
-  lft.setAttribute('do-something-once',{mydata:[1,-1]});
-  sceneEl.appendChild(lft);
- 
- // }
+  }).call(this,i);
+  }
 
 
 
