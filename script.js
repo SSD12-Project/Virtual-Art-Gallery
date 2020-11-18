803e3576c488873fcde100b53d98d0e675dfa71e
Parth311 <49272208+Parth311@users.noreply.github.com>
2020-11-18 23:39:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ var sceneEl = document.querySelector('a-scene');
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     curr1.object3D.position.set(pos, 0, (this.data.mydata[0]));
     //entityEl.object3D.position.z += (3*-35);
-    curr1.object3D.rotation.y = THREE.Math.degToRad(this.data.mydata[3]);
+    curr1.object3D.rotation.y = THREE.Math.degToRad(-1*this.data.mydata[3]);
     curr1.setObject3D('mesh', this.mesh);
     
       console.log('I am ready!');
@@ -58,7 +58,7 @@ var sceneEl = document.querySelector('a-scene');
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     lft1.object3D.position.set(pos, 0, (this.data.mydata[0]));
     //entityEl.object3D.position.z += (3*-35);
-    lft1.object3D.rotation.y = THREE.Math.degToRad(this.data.mydata[3]);
+    lft1.object3D.rotation.y = THREE.Math.degToRad(-1*this.data.mydata[3]);
     lft1.setObject3D('mesh', this.mesh);
     
       console.log('I am ready!');
@@ -85,7 +85,7 @@ var sceneEl = document.querySelector('a-scene');
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     curr2.object3D.position.set(pos, 0, (this.data.mydata[0]));
     //entityEl.object3D.position.z += (3*-35);
-    curr2.object3D.rotation.y = THREE.Math.degToRad(this.data.mydata[3]);
+    curr2.object3D.rotation.y = THREE.Math.degToRad(-1*this.data.mydata[3]);
     curr2.setObject3D('mesh', this.mesh);
     
       console.log('I am ready!');
@@ -112,7 +112,7 @@ var sceneEl = document.querySelector('a-scene');
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     lft2.object3D.position.set(pos, 0,(this.data.mydata[0]));
     //entityEl.object3D.position.z += (3*-35);
-    lft2.object3D.rotation.y = THREE.Math.degToRad(this.data.mydata[3]);
+    lft2.object3D.rotation.y = THREE.Math.degToRad(-1*this.data.mydata[3]);
     lft2.setObject3D('mesh', this.mesh);
     
       console.log('I am ready!');
@@ -139,7 +139,7 @@ var sceneEl = document.querySelector('a-scene');
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     curr3.object3D.position.set(pos, 0, (this.data.mydata[0]));
     //entityEl.object3D.position.z += (3*-35);
-    curr3.object3D.rotation.y = THREE.Math.degToRad(this.data.mydata[3]);
+    curr3.object3D.rotation.y = THREE.Math.degToRad(-1*this.data.mydata[3]);
     curr3.setObject3D('mesh', this.mesh);
     
       console.log('I am ready!');
@@ -166,7 +166,7 @@ var sceneEl = document.querySelector('a-scene');
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     lft3.object3D.position.set(pos, 0, (this.data.mydata[0]));
     //entityEl.object3D.position.z += (3*-35);
-    lft3.object3D.rotation.y = THREE.Math.degToRad(this.data.mydata[3]);
+    lft3.object3D.rotation.y = THREE.Math.degToRad(-1*this.data.mydata[3]);
     lft3.setObject3D('mesh', this.mesh);
     
       console.log('I am ready!');
@@ -192,32 +192,37 @@ var sceneEl = document.querySelector('a-scene');
     var z1l = (wall_coor[1]['z']+wall_coor[0]['z'])/2;
     var w1l = Math.sqrt(Math.pow((wall_coor[1]['x']-wall_coor[0]['x']),2) + Math.pow((wall_coor[1]['z']-wall_coor[0]['z']),2));
     var s1l = (Math.atan((wall_coor[1]['z']-wall_coor[0]['z'])/(wall_coor[1]['x']-wall_coor[0]['x'])))*(180/Math.PI);
+    //var s1l=angle_rot[0]
    
     var x1r = (wall_coor[2]['x']+wall_coor[0]['x'])/2;
     var z1r = (wall_coor[2]['z']+wall_coor[0]['z'])/2;
     var w1r = Math.sqrt(Math.pow((wall_coor[2]['x']-wall_coor[0]['x']),2) + Math.pow((wall_coor[2]['z']-wall_coor[0]['z']),2));
     var s1r = (Math.atan((wall_coor[2]['z']-wall_coor[0]['z'])/(wall_coor[2]['x']-wall_coor[0]['x'])))*(180/Math.PI);
-    
+    //var s1r=angle_rot[0]
+
     var x2l = (wall_coor[3]['x']+wall_coor[1]['x'])/2;
     var z2l = (wall_coor[3]['z']+wall_coor[1]['z'])/2;
     var w2l = Math.sqrt(Math.pow((wall_coor[3]['x']-wall_coor[1]['x']),2) + Math.pow((wall_coor[3]['z']-wall_coor[1]['z']),2));
     var s2l = (Math.atan((wall_coor[3]['z']-wall_coor[1]['z'])/(wall_coor[3]['x']-wall_coor[1]['x'])))*(180/Math.PI);
+    //var s2l=angle_rot[1]
 
     var x2r = (wall_coor[4]['x']+wall_coor[2]['x'])/2;
     var z2r = (wall_coor[4]['z']+wall_coor[2]['z'])/2;
     var w2r = Math.sqrt(Math.pow((wall_coor[4]['z']-wall_coor[2]['z']),2) + Math.pow((wall_coor[4]['x']-wall_coor[2]['x']),2));
     var s2r = (Math.atan((wall_coor[4]['z']-wall_coor[2]['z'])/(wall_coor[4]['x']-wall_coor[2]['x'])))*(180/Math.PI);
+    //var s2r=angle_rot[1]
 
     var x3l = (wall_coor[5]['x']+wall_coor[3]['x'])/2;
     var z3l = (wall_coor[5]['z']+wall_coor[3]['z'])/2;
     var w3l = Math.sqrt(Math.pow((wall_coor[5]['x']-wall_coor[3]['x']),2) + Math.pow((wall_coor[5]['z']-wall_coor[3]['z']),2));
     var s3l = (Math.atan((wall_coor[5]['z']-wall_coor[3]['z'])/(wall_coor[5]['x']-wall_coor[3]['x'])))*(180/Math.PI);
+    //var s3l=angle_rot[2]
 
     var x3r = (wall_coor[6]['x']+wall_coor[4]['x'])/2;
     var z3r = (wall_coor[6]['z']+wall_coor[4]['z'])/2;
     var w3r = Math.sqrt(Math.pow((wall_coor[6]['x']-wall_coor[4]['x']),2) + Math.pow((wall_coor[6]['z']-wall_coor[4]['z']),2));
     var s3r = (Math.atan((wall_coor[6]['z']-wall_coor[4]['z'])/(wall_coor[6]['x']-wall_coor[4]['x'])))*(180/Math.PI);
-
+    //var s3r=angle_rot[2]
 
     curr1.setAttribute('do-something-once-loaded-1',{mydata:[z1l,x1l,w1l,s1l]});
     sceneEl.appendChild(curr1);
@@ -246,6 +251,7 @@ var sceneEl = document.querySelector('a-scene');
 
 
   wall_coor=[]
+  angle_rot=[]
   var slope;
   store_cord(0,0,wall_coor)
   roadwidth=10
@@ -254,56 +260,41 @@ var sceneEl = document.querySelector('a-scene');
   function normalize(forward,slope)
   {
       var val = Math.sqrt(1/(1+Math.pow(slope,2)));
-     /* x=forward.x
+      x=forward.x
       z=forward.z
       a=Math.sqrt(Math.pow(x,2)+Math.pow(z,2))
       forward.x= forward.x/a
-      forward.z=forward.z/a*/
-      forward.x = val;
-      forward.z = slope*val;
+      forward.z=forward.z/a
+      /*forward.x = val;
+      forward.z = slope*val;*/
       return forward
   }
   function find_cor(points)
   {
       for (var i=0;i<points.length;i++)
       {
-         console.log('incoder');
-         if(i==0){
-           slope = points[i+1].z / points[i+1].x;
-           slope = -1*(1/slope);
-         }
-         else if(i==points.length - 1){
-           slope = (points[i].z - points[i-1].z)/(points[i].x - points[i-1].x);
-         }
-         else{
-          var m1 = (points[i].z - points[i-1].z)/(points[i].x - points[i-1].x);
-          var m2 = (points[i+1].z - points[i].z)/(points[i+1].x - points[i].x);
-           slope = (m1*m2 - 1 - (Math.sqrt((Math.pow(m1,2)+1)*(Math.pow(m2,2)+1))))/(m1+m2);
-         }
-       /*   if(i<points.length-1)
-          {
-              forward.x+=(points[i+1].x-points[i].x);
-              forward.z+=(points[i+1].z-points[i].z);
-          }
-          if (i>0)
-          {
-              forward.x+=points[i].x-points[i-1].x;
-              forward.z+=points[i].z-points[i-1].z;
-          }*/
-          forward=normalize(forward,slope);
-         /* left.x=-forward.z
-          left.z=forward.x 
-          //console.log(left) 
-          a=points[i].x - (left.x*roadwidth*0.5) 
-          b=points[i].z - (left.z*roadwidth*0.5) */
-          a = points[i].x - (roadwidth*0.5*forward.x);
-          b = points[i].z - (roadwidth*0.5*forward.z);
-          store_cord(a,b,wall_coor)  
-        /*  a=points[i].x + (left.x*roadwidth*0.5) 
-          b=points[i].z + (left.z*roadwidth*0.5) */
-          a = points[i].x + (roadwidth*0.5*forward.x);
-          b = points[i].z + (roadwidth*0.5*forward.z);
-          store_cord(a,b,wall_coor)   
+
+            if(i<points.length-1)
+            {
+                forward.x+=(points[i+1].x-points[i].x);
+                forward.z+=(points[i+1].z-points[i].z);
+            }
+            if (i>0)
+            {
+                forward.x+=points[i].x-points[i-1].x;
+                forward.z+=points[i].z-points[i-1].z;
+            }
+            forward=normalize(forward)
+            left.x=-forward.z
+            left.z=forward.x 
+            //console.log(left) 
+            a=points[i].x - (left.x*roadwidth*0.5) 
+            b=points[i].z - (left.z*roadwidth*0.5) 
+            store_cord(a,b,wall_coor)  //Left Coordinates
+            a=points[i].x + (left.x*roadwidth*0.5) 
+            b=points[i].z + (left.z*roadwidth*0.5) 
+            store_cord(a,b,wall_coor)   //Right Coordinates
+        
       }
       console.log('wall_cordinate: ',wall_coor);
       build_wall(wall_coor);
@@ -316,6 +307,7 @@ function apif(){
     var x = 0
     var z = 0
     var beta = 1
+    angle_rot.push(beta);
     var length = 10
     var newbeta = 1
     var response    //For retrieving API response 
@@ -347,6 +339,7 @@ function apif(){
     beta = response.new_beta
     x = response.new_point[0]
     z = response.new_point[1]
+    angle_rot.push(beta)
     console.log('beta:', beta)
     console.log('x: ', x)
     console.log('z:', z)
